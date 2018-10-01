9a82dffb8765dd89be3633c89fabfa9b2940329b
LucioTrincheri <lucio4852071@gmail.com>
2018-10-01 20:36:57 -0300

    Arreglo comportamiento inadecuado de los tests

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -8,19 +8,19 @@ class CartaTest extends TestCase {
 
 	public function testPalo(){
 		$carta = new Carta("9", "Treboles");
-		$this->assertEquals($carta->palo(), "Corazones" || "Treboles" || "Picas" || "Diamantes" || "Bastones" || "Copas" || "Espadas" || "Oro");
+		$this->assertTrue($carta->isPalo());
 	}
 
 
 	public function testNumero(){
 		$carta = new Carta("9", "Treboles");
-		$this->assertFalse(isalpha($carta->numero()));
+		$this->assertFalse(is_int($carta->numero()));
 	}
 
 
 	public function testTipo(){
 		$carta = new Carta("9", "Treboles");
-		$this->assertEquals($carta->tipo(),"Poker"||"Españolas");
+		$this->assertEquals($carta->tipo(),"Poker");
 	}
 
 }
