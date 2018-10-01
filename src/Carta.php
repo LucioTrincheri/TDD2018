c1908d24012dd1eb59c9b0a9e0152329ee0637a6
LucioTrincheri <lucio4852071@gmail.com>
2018-10-01 20:38:05 -0300

    Corrijo y creo nuevos metodos que satisfagan los nuevos tests

--- src/Carta.php
+++ src/Carta.php
@@ -6,20 +6,27 @@ class Carta {
 
 	protected $numero;
 	protected $palo;
+	protected $tipo;
 
 	public function __construct($numero=NULL, $palo=NULL){
 		$this->numero = $numero;
 		$this->palo = $palo;
 		if($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones"){
-			$this->tipo = "Poker"
+			$this->tipo = "Poker";
 		}else{
-			$this->tipo = "Españolas"
+			$this->tipo = "Españolas";
 		}
 	}
 
 	public function palo(){
 		return $this->palo;	
 	}
+
+	public function isPalo(){
+		return ($this->tipo == "Poker" || $this->tipo == "Españolas");
+	}
+
+	
 
 	public function numero(){
 		return $this->numero;
--- src/Mazo.php
+++ src/Mazo.php
@@ -3,7 +3,6 @@
 namespace TDD;
 
 class Mazo {
-
 	protected mazo;
 	protected cantidad;
 	protected tipo;
