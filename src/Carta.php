7c26b9dc799ab04ea9f363899be3e83151d15c77
LucioTrincheri <lucio4852071@gmail.com>
2018-10-01 20:15:06 -0300

    Agrego metodos a la clase carta

--- src/Carta.php
+++ src/Carta.php
@@ -10,5 +10,22 @@ class Carta {
 	public function __construct($numero=NULL, $palo=NULL){
 		$this->numero = $numero;
 		$this->palo = $palo;
+		if($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones"){
+			$this->tipo = "Poker"
+		}else{
+			$this->tipo = "Españolas"
+		}
+	}
+
+	public function palo(){
+		return $this->palo;	
+	}
+
+	public function numero(){
+		return $this->numero;
+	}
+
+	public function tipo(){
+		return $this->tipo;
 	}
 }
